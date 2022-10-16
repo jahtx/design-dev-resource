4ac422e1f6c9f4adebd40103e347034c218e551f
James Hernandez <main@jahtx.com>
2022-10-15 22:33:21 -0500

    go

---
+++ data/siteConfig.ts
@@ -0,0 +1,22 @@
+const siteConfig = {
+  title: "Work Design Dev",
+  keywords: "user experience, front-end development, react, redux",
+  author: "James Hernandez",
+  siteUrl: "https://workdesigndev.com",
+  siteDescription: "A blog about user experience and front-end web development",
+  social: {
+    twitter: "hernandezjames",
+  },
+  siteRss: "/rss.xml",
+  siteRssTitle: "Work-Design-Dev RSS feed",
+  googleAnalyticsID: "UA-xxx",
+  dateFromFormat: "YYYY-MM-DD",
+  dateFormat: "DD/MM/YYYY",
+  userName: "James",
+  userEmail: "contact@jahtx.com",
+  userTwitter: "hernandezjames",
+  userLocation: "Seguin, TX",
+  copyright: `Copyright © 2022. James Hernandez`,
+};
+
+export default siteConfig;
--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,13 +3,27 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Work Design Dev`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass"]
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ],
 };
 
 export default config;
