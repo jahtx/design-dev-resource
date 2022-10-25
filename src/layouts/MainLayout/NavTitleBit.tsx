6dd823d30550fd358aaba02cfd142700b88f8219
James Hernandez <main@jahtx.com>
2022-10-24 20:40:12 -0500

    go

--- src/layouts/MainLayout/NavTitleBit.tsx
+++ src/layouts/MainLayout/NavTitleBit.tsx
@@ -19,6 +19,13 @@ const NavTitleBit = () => {
 
   const handleToggle = () => {
     setActive(!isActive);
+    var growDiv = document.getElementById("mainHamper");
+    if (growDiv.clientHeight === 300) {
+      growDiv.style.height = "300";
+    } else {
+      var wrapper = document.querySelector(".measuringWrapper");
+      growDiv.style.height = wrapper.clientHeight + "px";
+    }
   };
   return (
     <>
@@ -26,59 +33,59 @@ const NavTitleBit = () => {
       <div className="ddrMenuMobl__cradle">
         {/* cradle determines whether the element shows in mobile or desktop sizes */}
         <header
-          className={
-            "ddrMenuMobl__hamper d-flex flex-column align-items-start m-0 p-0 " +
-            (isActive ? "ddrMenuMobl__hamper--isActive" : null)
-          }
+          id="mainHamper"
+          className="ddrMenuMobl__hamper d-flex flex-column align-items-start m-0 p-0"
         >
-          <nav className="ddrMenuMobl__headerBar d-flex align-items-center justify-content-center">
-            <div className="ddrMenuMobl__hamburgerCont d-flex align-items-center justify-content-center">
-              <button
-                className={
-                  "hamburger hamburger--vortex-r " +
-                  (isActive ? "is-active" : null)
-                }
-                onClick={handleToggle}
-              >
-                <span className="hamburger-box">
-                  <span className="hamburger-inner"></span>
-                </span>
-              </button>
-            </div>
-            <h1 className="m-0 p-0 d-flex align-items-center justify-content-center">
-              <div className="ddrMenuMobl__imgCont d-flex align-items-center justify-content-center">
-                <GatsbyImage
-                  image={getImage(data.ddrLogoMobile)}
-                  alt="Design Dev Resource"
-                  objectFit="contain"
-                  className="ddrMenuMobl__img"
-                />
+          <div className="measuringWrapper">
+            <nav className="ddrMenuMobl__headerBar d-flex align-items-center justify-content-center">
+              <div className="ddrMenuMobl__hamburgerCont d-flex align-items-center justify-content-center">
+                <button
+                  className={
+                    "hamburger hamburger--vortex-r " +
+                    (isActive ? "is-active" : null)
+                  }
+                  onClick={handleToggle}
+                >
+                  <span className="hamburger-box">
+                    <span className="hamburger-inner"></span>
+                  </span>
+                </button>
               </div>
-            </h1>
-          </nav>
-          <ul
-            className={
-              "ddrMenuMobl__list text-center m-0 p-0 " +
-              (isActive ? "ddrMenuMobl__list--isActive" : null)
-            }
-          >
-            {navItems &&
-              navItems.map((navItem) => {
-                const { linkName, url, ariaLabel } = navItem;
-                const id = nanoid();
-                return (
-                  <li key={id}>
-                    <a
-                      href={url}
-                      aira-label={ariaLabel}
-                      onClick={() => setActive(!isActive)}
-                    >
-                      {linkName}
-                    </a>
-                  </li>
-                );
-              })}
-          </ul>
+              <h1 className="m-0 p-0 d-flex align-items-center justify-content-center">
+                <div className="ddrMenuMobl__imgCont d-flex align-items-center justify-content-center">
+                  <GatsbyImage
+                    image={getImage(data.ddrLogoMobile)}
+                    alt="Design Dev Resource"
+                    objectFit="contain"
+                    className="ddrMenuMobl__img"
+                  />
+                </div>
+              </h1>
+            </nav>
+            <ul
+              className={
+                "ddrMenuMobl__list text-center m-0 p-0 " +
+                (isActive ? "ddrMenuMobl__list--isActive" : null)
+              }
+            >
+              {navItems &&
+                navItems.map((navItem) => {
+                  const { linkName, url, ariaLabel } = navItem;
+                  const id = nanoid();
+                  return (
+                    <li key={id}>
+                      <a
+                        href={url}
+                        aira-label={ariaLabel}
+                        onClick={() => setActive(!isActive)}
+                      >
+                        {linkName}
+                      </a>
+                    </li>
+                  );
+                })}
+            </ul>
+          </div>
         </header>
       </div>
       {/* DESKTOP MENU */}
